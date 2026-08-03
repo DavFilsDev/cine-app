3c13ce14e1038699dba1ff570e9c765369ab10bd
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-03 21:39:49 +0300

    feat(repository): create JReservationRepository with extends Jparepository

---
+++ src/main/java/school/hei/cineapp/repository/JReservationRepository.java
@@ -0,0 +1,11 @@
+package school.hei.cineapp.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import school.hei.cineapp.repository.model.JReservation;
+
+public interface JReservationRepository extends JpaRepository<JReservation, UUID> {
+  List<JReservation> findByUserId(String userId);
+
+  boolean existsByProjectionIdAndSeatId(String projectionId, String seatId);
+}
