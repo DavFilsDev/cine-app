3dfd6d1d277eff44791423b12993ddaa5ee93152
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-04 10:25:10 +0300

    feat(repository):JReservationRepo add String in the paremetres with JReservation

--- src/main/java/school/hei/cineapp/repository/JReservationRepository.java
+++ src/main/java/school/hei/cineapp/repository/JReservationRepository.java
@@ -1,6 +1,7 @@
 package school.hei.cineapp.repository;
 
 import java.util.List;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import school.hei.cineapp.repository.model.JReservation;
 
