871214bbb2f18cc358728327578ab4d35fdb0db5
Fenohasina08 <fenohasina.ra_fanomezana@student.passerellesnumerIques.org>
2026-08-07 10:50:30 +0300

    feat(endpoint): add reservation ticket to endpoint

--- src/main/java/school/hei/cineapp/endpoint/rest/controller/ReservationController.java
+++ src/main/java/school/hei/cineapp/endpoint/rest/controller/ReservationController.java
@@ -4,6 +4,9 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import school.hei.cineapp.file.ticket.TicketGenerator;
 import school.hei.cineapp.model.Reservation;
 import school.hei.cineapp.security.model.Principal;
 import school.hei.cineapp.service.ReservationService;
@@ -20,6 +24,7 @@ import school.hei.cineapp.service.ReservationService;
 @RequestMapping(produces = APPLICATION_JSON_VALUE)
 public class ReservationController {
   private final ReservationService service;
+  private final TicketGenerator ticketGenerator;
 
   @GetMapping("/reservations")
   public List<Reservation> getAllReservations() {
@@ -30,6 +35,18 @@ public class ReservationController {
   public Reservation getReservationById(
       @PathVariable String reservationId, @AuthenticationPrincipal Principal principal) {
     return service.getById(reservationId, principal);
+  }
+
+  @GetMapping(value = "/reservations/{reservationId}/ticket", produces = MediaType.ALL_VALUE)
+  public ResponseEntity<byte[]> downloadTicket(
+      @PathVariable String reservationId, @AuthenticationPrincipal Principal principal) {
+    var ticket = service.buildTicketFor(reservationId, principal);
+    var file = ticketGenerator.generate(ticket);
+
+    return ResponseEntity.ok()
+        .contentType(file.contentType())
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.filename() + "\"")
+        .body(file.content());
   }
 
   @PutMapping("/reservation")
